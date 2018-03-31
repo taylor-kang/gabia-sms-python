d799d39b2aa67bcb78e0f0de50b55489ee6a8b71
hyunwoo.shim <shim.hw0810@gmail.com>
2018-03-31 20:22:49 +0900

    Modified send func params

--- gabia_sms/core.py
+++ gabia_sms/core.py
@@ -78,8 +78,8 @@ class GabiaSMS:
         """
         pass
 
-    def __get_sms_param(self, message, receiver, scheduled_time,
-                        sms_type='sms', title='SEND'):
+    def __get_sms_param(self, message, receiver, title, scheduled_time,
+                        sms_type='sms'):
 
         self.__validate_required_params(message, receiver)
 
@@ -93,12 +93,13 @@ class GabiaSMS:
             'key': int(timezone.now().timestamp())
         }
 
-    def send(self, receiver, message,
+    def send(self, message, receiver, title='SEND',
              sms_type='sms', scheduled_time='0', *args, **kwargs):
         """
         Use for send SMS\n
-        :param receiver: Receive Phone number
         :param message: Message
+        :param receiver: Receive Phone number
+        :param title: SMS TITLE(DEFAULT VALUE: 'SEND') : Used where the sms_type is 'lms'
         :param sms_type: ref KNOWN_SMS_TYPES: default is sms
         :param scheduled_time: default 0: send immediately or '%Y-%M-%D %h:%m:%s'
         """
@@ -106,7 +107,7 @@ class GabiaSMS:
             raise SMSModuleException('Please check sms type!')
 
         self.__send_single(
-            self.__get_sms_param(message, receiver, scheduled_time),
+            self.__get_sms_param(message, receiver, title, scheduled_time),
             *args,
             **kwargs
         )
--- gabia_sms/shortcuts.py
+++ gabia_sms/shortcuts.py
@@ -1,12 +1,13 @@
 from .core import GabiaSMS
 
 
-def send(receiver, message, sms_type='sms', scheduled_time='0', *args, **kwargs):
+def send(message, receiver, title='SEND', sms_type='sms', scheduled_time='0', *args, **kwargs):
     """
     Shortcut function for send SMS\n
-    :param receiver: Receive Phone number
     :param message: Message
+    :param receiver: Receive Phone number
+    :param title: SMS TITLE(DEFAULT VALUE: 'SEND') : Used where the sms_type is 'lms'
     :param sms_type: ref KNOWN_SMS_TYPES: default is sms
     :param scheduled_time: default 0: send immediately or '%Y-%M-%D %h:%m:%s'
     """
-    GabiaSMS().send(receiver, message, sms_type, scheduled_time, *args, **kwargs)
+    GabiaSMS().send(message, receiver, title, sms_type, scheduled_time, *args, **kwargs)
