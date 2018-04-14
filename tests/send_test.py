ace6d92967a439afc367d3c25631be10d7df565f
hyunwoo.shim <shim.hw0810@gmail.com>
2018-04-14 15:00:59 +0900

    Add send empty message test

--- tests/send_test.py
+++ tests/send_test.py
@@ -12,13 +12,13 @@ class SendTestCase(TestCase):
     def __get_module(cls):
         return gabia_sms.SingletonGabiaSMS()
 
-    def test_invalid_send_type(self):
-        with self.assertRaises(gabia_sms.SMSModuleException):
-            self.__get_module().send(self.test_message, self.test_receiver,
-                                     sms_type='invalid')
-
     def test_invalid_sent_type_error_message(self):
         with self.assertRaisesMessage(gabia_sms.SMSModuleException,
                                       'Please check sms type!'):
             self.__get_module().send(self.test_message, self.test_receiver,
                                      sms_type='invalid')
+
+    def test_empty_message_error_message(self):
+        with self.assertRaisesMessage(gabia_sms.SMSModuleException,
+                                      'Please check required parameters!'):
+            self.__get_module().send('', self.test_receiver)
