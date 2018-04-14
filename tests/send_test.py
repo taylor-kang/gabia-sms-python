f1faa357639606be98971b423103c6b31b424be3
hyunwoo.shim <shim.hw0810@gmail.com>
2018-04-14 14:58:24 +0900

    Add test util method

--- tests/send_test.py
+++ tests/send_test.py
@@ -8,17 +8,17 @@ class SendTestCase(TestCase):
     test_message = 'Test Message'
     test_receiver = '01000000000'
 
-    def test_invalid_send_type(self):
-        module = gabia_sms.SingletonGabiaSMS()
+    @classmethod
+    def __get_module(cls):
+        return gabia_sms.SingletonGabiaSMS()
 
+    def test_invalid_send_type(self):
         with self.assertRaises(gabia_sms.SMSModuleException):
-            module.send(self.test_message, self.test_receiver,
-                        sms_type='invalid')
+            self.__get_module().send(self.test_message, self.test_receiver,
+                                     sms_type='invalid')
 
     def test_invalid_sent_type_error_message(self):
-        module = gabia_sms.SingletonGabiaSMS()
-
         with self.assertRaisesMessage(gabia_sms.SMSModuleException,
                                       'Please check sms type!'):
-            module.send(self.test_message, self.test_receiver,
-                        sms_type='invalid')
+            self.__get_module().send(self.test_message, self.test_receiver,
+                                     sms_type='invalid')
